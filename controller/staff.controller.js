aade31c693e51a0f05afccabe44b0eb25844333b
PremPrakash504 <tripbad741@gmail.com>
2025-12-22 08:40:56 +0545

    Dec 22 2025

--- controller/staff.controller.js
+++ controller/staff.controller.js
@@ -12,11 +12,19 @@ export const addStaff = async (req, res) => {
       branch_id,
     } = req.body;
     console.log(req.body);
-    if (!name || !email) {
+    console.log(req.file);
+    if (!name || !email || !branch_id || phone ) {
       return res.status(400).json({
         message: "All fields are required",
       });
     }
+    const [branch]= await db.query("SELECT* FROM branch WHERE branch_id=?", [
+      branch_id,
+    ]);
+    if(branch/length==0){
+      return res.status(403)({message:"Branch not found"})
+    }
+    
   } catch (error) {
     console.log("Internal server error occour", error);
   }
